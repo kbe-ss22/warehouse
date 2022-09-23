777b8a3ef33daf75613f7f014376ec23c905d7c2
Lincoln Musah <Lincoln.Musah@gmail.com>
2022-09-23 18:20:58 +0200

    KBES22-33 create Objects and save into Database

--- src/main/java/com/kbe/warehouse/LoadCsvToDB.java
+++ src/main/java/com/kbe/warehouse/LoadCsvToDB.java
@@ -3,21 +3,28 @@ package com.kbe.warehouse;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class LoadCsvToDB implements CommandLineRunner {
 
+    static final String HARDWAREFILEPATH = "src/main/resources/Hardware.csv";
+    static final String PRODUCTFILEPATH = "src/main/resources/Product.csv";
+    private static HardwareRepository hardwareRepository;
+    private static ProductRepository productRepository;
+    private static List<Hardware> hardwareList = new ArrayList<>();
 
-    HardwareRepository hardwareRepository;
-    ProductRepository productRepository;
 
     public LoadCsvToDB(HardwareRepository hardwareRepository, ProductRepository productRepository){
         this.hardwareRepository = hardwareRepository;
         this.productRepository = productRepository;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
@@ -25,43 +32,87 @@ public class LoadCsvToDB implements CommandLineRunner {
     }
 
     private void loadAndSave(){
-        //load data fromcsv
-        //loadDataFromCsv();
-        //save data to db
-        //saveDataToDB();
+        loadHardwareFromCsv();
+        //System.out.println(loadProductFromCsv());
+        saveDataToDB(loadProductFromCsv());
     }
 
-    private void loadDataFromCsv(){
+    private void loadHardwareFromCsv(){
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(HARDWAREFILEPATH))) {
 
+            br.readLine();
+            String line = br.readLine();
+
+            while (line != null) {
+                String[] attributes = line.split(";");
+                Hardware hardware = createHardware(attributes);
+                hardwareList.add(hardware);
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 
-    private void saveDataToDB(){
-        Hardware cpu = new Hardware();
-        //cpu.setId(1);
-        cpu.setName("ryzen7");
-        cpu.setType("cpu");
-        cpu.setDescription("8 cores");
-        cpu.setPrice(222.52);
-        cpu.setStock(5);
+    private List<Product> loadProductFromCsv() {
 
-        Hardware gpu = new Hardware();
-        //cpu.setId(1);
-        cpu.setName("rx 6800");
-        cpu.setType("gpu");
-        cpu.setDescription("fast");
-        cpu.setPrice(222.42);
-        cpu.setStock(5);
+        List<Product> productList = new ArrayList<>();
 
-        Product pc = new Product();
-        pc.setName("wunder");
-        List<Hardware> hardwareList = new ArrayList<>();
-        hardwareList.add(cpu);
-        hardwareList.add(gpu);
-        pc.setHardware(hardwareList);
+        try (BufferedReader br = Files.newBufferedReader(Paths.get(PRODUCTFILEPATH))) {
 
-        hardwareRepository.save(cpu);
-        hardwareRepository.save(gpu);
+            br.readLine();
+            String line = br.readLine();
 
-        productRepository.save(pc);
+            while (line != null) {
+                String[] attributes = line.split(";");
+                Product product = createProduct(attributes);
+                productList.add(product);
+                line = br.readLine();
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        return productList;
+    }
+
+    private static Hardware createHardware(String[] metadata) {
+
+        int id = Integer.parseInt(metadata[0]);
+        String name = metadata[1];
+        String description = formatComponentDescription(metadata[2]);
+        float price = Float.parseFloat(metadata[3]);
+        int stock = Integer.parseInt(metadata[4]);
+        HardwareType productType = HardwareType.valueOf(metadata[5].toUpperCase());
+
+        return new Hardware(id, productType, name , description, price, stock);
+    }
+
+    private static Product createProduct(String[] metadata) {
+
+        int id = Integer.parseInt(metadata[0]);
+        String name = metadata[1];
+        List<Hardware> hardware = new ArrayList<>();
+
+        for(int i = 2; i < metadata.length; i++){
+
+            hardware.add( hardwareList.get(Integer.parseInt(metadata[i])));
+        }
+        return new Product(id,name,hardware);
+    }
+
+    private static String formatComponentDescription(String str)
+    {
+        String newStr = str.replaceAll("\"","").replaceAll("/"," " +
+                "");
+
+        return String.join(" ",newStr.split("\""));
+    }
+
+    private void saveDataToDB(List<Product> productList){
+
+        hardwareRepository.saveAll(hardwareList);
+        productRepository.saveAll(productList);
+
     }
 }
